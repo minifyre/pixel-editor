235af7c0055d4472de479605e7cb22a898e4d248
Minifyre <minifyre@gmail.com>
2018-10-04 17:26:32 -0500

    +colorSelect
    Moved color selection code into logic.

--- logic.mjs
+++ logic.mjs
@@ -9,4 +9,9 @@ logic.draw=function(state,x,y,type=0)
 	state.pts[x+','+y]=state.selectedColors[type]
 }
 logic.colorEdit=(state,color)=>state.editColor=color
+logic.colorSelect=function(state,id,type)
+{
+	if(id===state.selectedColors[type]) logic.colorEdit(state,id)//edit
+	else state.selectedColors[type]=id//select
+}
 export default silo
