f67fd127c03bdd0558a1ffd04869ea62f791c783
Minifyre <minifyre@gmail.com>
2018-10-04 17:27:01 -0500

    +colorAdd
    Added logic for adding a color (& the option to select it right away).

--- logic.mjs
+++ logic.mjs
@@ -8,6 +8,11 @@ logic.draw=function(state,x,y,type=0)
 	//@todo enable different tools
 	state.pts[x+','+y]=state.selectedColors[type]
 }
+logic.colorAdd=function(state,color,type=-1)
+{
+	const id=state.palette.push(color)-1
+	if(type!==-1) logic.colorSelect(state,id,type)
+}
 logic.colorEdit=(state,color)=>state.editColor=color
 logic.colorSelect=function(state,id,type)
 {
