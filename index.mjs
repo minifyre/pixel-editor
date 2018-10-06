30f6f5ab2d3fcf321351b09a965ad217e279fa9f
Minifyre <minifyre@gmail.com>
2018-10-06 17:23:23 -0500

    used v.render
    Got rid of redundant custom code & switched rendering over to v.render().

--- index.mjs
+++ index.mjs
@@ -18,14 +18,11 @@ pixel.editor=class extends HTMLElement
 	{
 		super()
 		const
-		shadow=this.attachShadow({mode:'open'}),
-		initalState=logic(state)
+		shadow=this.attachShadow({mode:'open'})
 		let renderer=x=>x
-		this.state=truth(initalState,(...args)=>renderer(args))
-		this.dom=output(this)
-		v.flatUpdate(shadow,this.dom)
-		renderer=()=>output.render(this)
+		this.state=truth(logic(state),(...args)=>renderer(args))
+		renderer=v.render(shadow,this,output)
+		//@todo find a better way to set this (render needs access to it, but canvas only exists after first render)
 		this.ctx=Object.assign(shadow.querySelector('canvas').getContext('2d'),{imageSmoothingEnabled:false})
-		//@todo copy resize observer from code-editor & integrate it here
 	}
 }
