7beccdebbf43a37dff797b8d8858a45f53374f7e
Minifyre <minifyre@gmail.com>
2018-10-07 13:36:40 -0500

    extended pane.viewer class
    Used a super class to attach the shadowRoot & gain access to the load property.

--- index.mjs
+++ index.mjs
@@ -9,17 +9,16 @@ export default async function pixel(url='/node_modules/pixel-editor/')
 	await color()
 }
 Object.assign(pixel,silo)
-pixel.editor=class extends HTMLElement
+pixel.editor=class extends silo.viewer
 {
 	constructor(state={})
 	{
-		super()
-		const
-		shadow=this.attachShadow({mode:'open'})
+		super(state)
 		let renderer=x=>x
 		this.state=truth(logic(state),(...args)=>renderer(args))
-		renderer=v.render(shadow,this,output)
-		//@todo find a better way to set this (render needs access to it, but canvas only exists after first render)
-		this.ctx=Object.assign(shadow.querySelector('canvas').getContext('2d'),{imageSmoothingEnabled:false})
+		renderer=v.render(this.shadowRoot,this,output)
+
+		this.ctx=this.shadowRoot.querySelector('canvas').getContext('2d')
+		this.ctx.imageSmoothingEnabled=false
 	}
 }
