84f27c01206c5fee1867267ae844c0694093256c
Minifyre <minifyre@gmail.com>
2018-10-20 13:04:24 -0500

    moved type into state.view

--- config.mjs
+++ config.mjs
@@ -12,9 +12,9 @@ config.state=
 		editColor:-1,
 		pointers:{},
 		selectedColors:{0:0},
+		type:'pixel-editor',
 		viewbox:{height:150,width:300,x:0,y:0}
 		//@todo get dimensions into state.view & have viewbox default to those
-	},
-	type:'pixel-editor'
+	}
 }
 export default silo
