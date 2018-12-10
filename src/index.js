e6b7cbb54b79eabcb3fc1195ba26e83ad6a53a6a
Minifyre <minifyre@gmail.com>
2018-12-10 11:54:27 -0600

    used new silo module

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import color from './node_modules/color-picker/index.mjs'
-import silo from './output.mjs'
-const
-{config,input,logic,output,util}=silo,
-{truth,v}=util
-export default async function pixel(url='/node_modules/pixel-editor/')
+import silo from './node_modules/silo/index.js'
+import truth from './node_modules/truth/truth.mjs'
+import v from './node_modules/v/v.mjs'
+
+const {config,util,logic,output,input}=silo
+
+export default silo(async function pixel(url='/node_modules/pixel-editor/')
 {
 	await util.mkCustomEl(url,'pixel-editor',pixel.editor)
 	await color()
-}
-Object.assign(pixel,silo)
-pixel.editor=class extends silo.viewer
+})
+pixel.editor=class extends silo.customElement
 {
 	constructor(state={})
 	{
